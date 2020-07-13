249e2ef28bc3008902ffb06d04b556552e754f65
k018c1397@it-neec.jp <k018c1397@it-neec.jp>
2020-07-13 13:16:17 +0900

    3.4template(if)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',obj = {"title":"タイトル2"})
+    return render_template('index.html',num = 7)
 
 if __name__ == '__main__':
     app.debug = True
